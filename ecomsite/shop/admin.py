4f39201828954fe461c7327dfd5327e7e1ff2dc5
Wray14 <ravi.wray@gmail.com>
2020-06-26 23:29:49 +0530

    Customised the admin panel

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Product, Order
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.site_header = "E-Commerce Site"
+admin.site.site_title = "Shopping portal"
+admin.site.index_title = "Manage shopping portal"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'discount_price', 'category', 'description',)
+    search_fields = ('title',)
+    list_editable = ('price', 'category',)
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
